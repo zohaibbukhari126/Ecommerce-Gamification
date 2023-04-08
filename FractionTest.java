a27078fa75809d1d059161fd9bf3831e0910d0e7
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2023-04-08 14:20:53 +0500

    Hi

---
+++ FractionTest.java
@@ -0,0 +1,17 @@
+public class FractionTest {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(2, 3);
+        f1.printFraction();
+
+        f1.addNumber(1);
+        f1.printFraction();
+
+        f1.simplifyFraction();
+        f1.printFraction();
+
+        Fraction f2 = new Fraction(6, 8);
+        f2.simplifyFraction();
+        f2.printFraction();
+    }
+    
+}
