3ec34475ad83bcd72e8fc7359b5d8611cb2f4016
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2023-04-08 15:50:04 +0500

    structural changes

---
+++ code/Fraction.java
@@ -0,0 +1,59 @@
+package code;
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        setDenominator(denominator);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            System.out.println("Denominator cannot be zero. Setting it to 1.");
+            this.denominator = 1;
+        } else {
+            this.denominator = denominator;
+        }
+    }
+
+    public void addNumber(int number) {
+        this.numerator += number * this.denominator;
+    }
+
+    public void simplifyFraction() {
+        int gcd = gcd(this.numerator, this.denominator);
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public void printFraction() {
+        System.out.println(this.numerator + "/" + this.denominator);
+    }
+}
--- code/FractionTest.java
+++ code/FractionTest.java
@@ -1,3 +1,4 @@
+package code;
 public class FractionTest {
     public static void main(String[] args) {
         Fraction f1 = new Fraction(2, 3);
